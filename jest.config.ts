5ede7d323e5d428f6aa09221ed233ddebc1b5996
Vivek <vivek.makoday@gmail.com>
2026-06-17 22:38:09 -0500

    fix: correct Jest config key setupFilesAfterEnv

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ import nextJest from 'next/jest.js'
 const createJestConfig = nextJest({ dir: './' })
 
 const config: Config = {
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
 }
